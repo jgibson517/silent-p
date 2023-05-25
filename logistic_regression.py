17f950813f698ad89e05192b72427cf544aa3d00
Benjamin Leiva <bleiva@linux1.cs.uchicago.edu>
2023-05-24 21:07:20 -0500

    LR module

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,85 +1,62 @@
 ################################################################################
 # LIBRARIES
 ################################################################################
-
 import torch
-import numpy as np
-from tqdm import tqdm
-from matplotlib import pyplot as plt
 import torch.nn as nn 
 import torch.optim as optim
 import torch.nn.functional as F 
-import torchvision.transforms as T
-from torchvision.io import read_image
-from torch.utils.data import Dataset, DataLoader
-# from process.model import CustomNeuralNetwork   #internal
-from process.data_module import CustomImageDataset  #internal
+from torch.utils.data import DataLoader
+from process.data_module import CustomImageDataset
 from process.transforms import base_transforms, edges_transforms, color_transforms, both_transforms 
-from process.load import collect_image_files
-from sklearn.metrics import recall_score, confusion_matrix
-
 
 ################################################################################
 # CONSTANTS
 ################################################################################
 
+labels = 2
 epochs = 15
-num_classes = 2
-#batch_size = 64
 image_size = 256*256
 learning_rate = 0.0001
-input_size = image_size 
 
 ################################################################################
 # GET DATA
 ################################################################################
 
-# Collect images
-collect_image_files('test')
-collect_image_files('train')
-collect_image_files('val')
-
 # Load original x-rays and apply transformations
-# training_data = CustomImageDataset("data2/paths/train.csv", "data2/train/", base_transforms)
-# val_data = CustomImageDataset("data2/paths/val.csv", "data2/val/", base_transforms)
-# test_data = CustomImageDataset("data2/paths/test.csv", "data2/test/", base_transforms)
+training_data = CustomImageDataset("data2/paths/train.csv", "data2/train/", base_transforms)
+val_data = CustomImageDataset("data2/paths/val.csv", "data2/val/", base_transforms)
+test_data = CustomImageDataset("data2/paths/test.csv", "data2/test/", base_transforms)
 
 # training_data = CustomImageDataset("data2/paths/train.csv", "data2/train/", edges_transforms)
 # val_data = CustomImageDataset("data2/paths/val.csv", "data2/val/", edges_transforms)
 # test_data = CustomImageDataset("data2/paths/test.csv", "data2/test/", edges_transforms)
 
-training_data = CustomImageDataset("data2/paths/train.csv", "data2/train/", color_transforms)
-val_data = CustomImageDataset("data2/paths/val.csv", "data2/val/", color_transforms)
-test_data = CustomImageDataset("data2/paths/test.csv", "data2/test/", color_transforms)
+# training_data = CustomImageDataset("data2/paths/train.csv", "data2/train/", color_transforms)
+# val_data = CustomImageDataset("data2/paths/val.csv", "data2/val/", color_transforms)
+# test_data = CustomImageDataset("data2/paths/test.csv", "data2/test/", color_transforms)
 
 # training_data = CustomImageDataset("data2/paths/train.csv", "data2/train/", both_transforms)
 # val_data = CustomImageDataset("data2/paths/val.csv", "data2/val/", both_transforms)
 # test_data = CustomImageDataset("data2/paths/test.csv", "data2/test/", both_transforms)
 
 
-
-# Load groups/batches of x-rays for analysis
+# # Load groups/batches of x-rays for analysis
 train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True, num_workers=3, pin_memory=True)
 val_dataloader = DataLoader(val_data, batch_size=64, shuffle=True, num_workers=3, pin_memory=True)
 test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True, num_workers=3, pin_memory=True)
-
-# Describe dataset
-print(len(training_data))
-img_tensor, label = training_data[0]
-print(img_tensor.shape, label)
 
 ################################################################################
 # CREATE LOGISTIC REGRESSION CLASS AND HELPER FUNCTIONS
 ################################################################################
 
-class LogisticRegression(torch.nn.Module):
+class LogisticRegression(nn.Module):
     """
+    This class creates a logistic regression model, that will serve as a 
+    baseline to compare the performance of our CNN model.
     """
     def __init__(self):
         super().__init__()
-        self.linear = nn.Linear(input_size, num_classes)
-
-#        print(self.linear.input)
+        self.linear = nn.Linear(image_size, labels)
 
     def forward(self, xb):
         xb = xb.view(xb.shape[0],-1)
@@ -99,19 +76,19 @@ class LogisticRegression(torch.nn.Module):
         loss = F.cross_entropy(out, labels) 
         acc = accuracy(out, labels)
         rec = recall(out, labels)
-        return {'val_loss': loss.detach(), 'val_acc': acc.detach(), 'val_rec': rec.detach()}
+        return {'loss': loss.detach(), 'accuracy': acc.detach(), 'recall': rec.detach()}
 
     def validation_epoch_end(self, outputs):
-        batch_losses = [x['val_loss'] for x in outputs]
+        batch_losses = [x['loss'] for x in outputs]
         epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
-        batch_accs = [x['val_acc'] for x in outputs]
+        batch_accs = [x['accuracy'] for x in outputs]
         epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
-        batch_recs = [x['val_rec'] for x in outputs]
+        batch_recs = [x['recall'] for x in outputs]
         epoch_rec = torch.stack(batch_recs).mean()      # Combine recalls
-        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item(), 'val_rec': epoch_rec.item()}
+        return {'loss': epoch_loss.item(), 'accuracy': epoch_acc.item(), 'recall': epoch_rec.item()}
     
-    def epoch_end(self, epoch, result):
-        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}, val_rec: {:.4f}".format(epoch, result['val_loss'], result['val_acc'], result['val_rec']))    
+    def epoch_end(self, epoch):
+        print("Epoch {}".format(epoch))    
 
 def accuracy(outputs, labels):
     _, preds = torch.max(outputs, dim=1)
@@ -127,8 +104,8 @@ def evaluate(model, val_loader):
     outputs = [model.validation_step(batch) for batch in val_loader]
     return model.validation_epoch_end(outputs)
 
-def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
-    history = []
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=optim.SGD):
+    metrics = []
     optimizer = opt_func(model.parameters(), lr)
     for epoch in range(epochs):
         # Training Phase 
@@ -139,9 +116,10 @@ def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
             optimizer.zero_grad()
         # Validation phase
         result = evaluate(model, val_loader)
-        model.epoch_end(epoch, result)
-        history.append(result)
-    return history
+        model.epoch_end(epoch)
+        if epoch == epochs-1:
+            metrics.append(result)
+    return metrics
 
 ################################################################################
 # INITIALIZE MODEL
@@ -149,118 +127,7 @@ def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
 
 model = LogisticRegression()
 
-for images, labels in train_dataloader:
-    # print("images.shape: " , images.shape)
-    # print("img_tensor.shape: ", img_tensor.shape)
-    images = images.type(torch.float32)
-    output = model(images)                                          #ERROR MESSAGE!!!
-    # break;
-print("output.shape: ", output.shape)
-print("output: ", output[:3].data)
+for images, _ in train_dataloader:
+    output = model(images.type(torch.float32))
 
-#Probabilities
-probs = F.softmax(output, dim=1)
-print("Probability: \n" ,probs.data)
-
-#Predictions
-maxprob, preds = torch.max(probs, dim=1)
-print(preds)
-print(maxprob)
-
-#Labels
-labels
-
-#Metrics
-accuracy(output, labels)
-recall(output, labels)
-
-#Loss
-loss_fn = F.cross_entropy
-loss = loss_fn(output, labels) 
-loss
-
-#Evaluate model
-evaluate(model, val_dataloader)
-#history = fit(epochs, learning_rate, model, training_data, val_dataloader)
-history = fit(epochs, learning_rate, model, train_dataloader, val_dataloader)
-accuracies = [r['val_acc'] for r in history]
-recalls = [r['val_rec'] for r in history]
-plt.plot(accuracies, '-x')
-plt.xlabel('epoch')
-plt.ylabel('accuracy')
-plt.title('Accuracy vs. No. of epochs')
-plt.show()
-
-################################################################################
-# END
-################################################################################
-
-# model = LogisticRegression(input_dim, output_dim)
-
-# # 4. Loss function + optimizer
-# criterion = torch.nn.BCELoss()
-# optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-# # Train model
-# X_train, X_test = torch.Tensor(X_train), torch.Tensor(X_test)
-# y_train, y_test = torch.Tensor(y_train), torch.Tensor(y_test)
-
-# losses_train = []
-# losses_test = []
-# Iterations = []
-# iter = 0
-# for epoch in tqdm(range(int(epochs)),desc='Training Epochs'):
-#     x = X_train
-#     labels = y_train
-#     optimizer.zero_grad() # Setting our stored gradients equal to zero
-#     outputs = model(X_train)
-#     loss = criterion(torch.squeeze(outputs), labels) # [200,1] -squeeze-> [200]
-    
-#     loss.backward() # Computes the gradient of the given tensor w.r.t. graph leaves 
-    
-#     optimizer.step() # Updates weights and biases with the optimizer (SGD)
-    
-#     iter+=1
-#     if iter%10000==0:
-#         # calculate Accuracy
-#         with torch.no_grad():
-#             # Calculating the loss and accuracy for the test dataset
-#             correct_test = 0
-#             total_test = 0
-#             outputs_test = torch.squeeze(model(X_test))
-#             loss_test = criterion(outputs_test, y_test)
-            
-#             predicted_test = outputs_test.round().detach().numpy()
-#             total_test += y_test.size(0)
-#             correct_test += np.sum(predicted_test == y_test.detach().numpy())
-#             accuracy_test = 100 * correct_test/total_test
-#             losses_test.append(loss_test.item())
-            
-#             # Calculating the loss and accuracy for the train dataset
-#             total = 0
-#             correct = 0
-#             total += y_train.size(0)
-#             correct += np.sum(torch.squeeze(outputs).round().detach().numpy() == y_train.detach().numpy())
-#             accuracy = 100 * correct/total
-#             losses_train.append(loss.item())
-#             Iterations.append(iter)
-            
-#             print(f"Iteration: {iter}. \nTest - Loss: {loss_test.item()}. Accuracy: {accuracy_test}")
-#             print(f"Train -  Loss: {loss.item()}. Accuracy: {accuracy}\n")
-
-#Hyperparameters
-# batch_size = 64
-# image_size = 256*256
-# learning_rate = 0.0001
-
-# #Other constants
-# input_size = image_size
-# num_classes = 2
-# epochs = 15
-# #input_dim = 2 # Two inputs x1 and x2 
-# #output_dim = 1 # Single binary output 
-
-# #Datasets
-# train_df = DataLoader
-# val_df = 
-# test_df = 
+fit(epochs, learning_rate, model, train_dataloader, test_dataloader)
